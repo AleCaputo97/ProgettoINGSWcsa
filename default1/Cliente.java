16ff5662aed3e0cc81a2c843c85348fded82e16d
Alessandro <alecaputo97@hotmail.it>
2018-01-20 14:57:53 +0100

    entity cliente aggiornata

--- default1/Cliente.java
+++ default1/Cliente.java
@@ -2,6 +2,53 @@ package default1;
 
 public class Cliente {
 	
-	//prova
+	//attributi
+	private String Nome;
+	private String Cognome;
+	private String Email;
+	private String CodiceFiscale;
+	private String Data;
+	
+	//getter
+	public String getNome() {
+		return Nome;
+	}
+	
+	public String getCognome() {
+		return Cognome;
+	}	
+	
+	public String getEmail() {
+		return Email;
+	}
+	
+	public String getCodiceFiscale() {
+		return CodiceFiscale;
+	}
+	
+	public String getData() {
+		return Data;
+	}
 
+	//setter
+	public void setNome(String nome) {
+		 Nome=nome;
+	}
+	
+	public void setCognome(String cognome) {
+		 Cognome=cognome;
+	}
+	
+	public void setEmail(String email) {
+		 Email=email;
+	}
+	
+	public void setCodiceFiscale(String codicefiscale) {
+		 CodiceFiscale=codicefiscale;
+	}
+	
+	public void setData(String data) {
+		 Data=data;
+	}
+	
 }
