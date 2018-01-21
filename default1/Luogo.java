68cd45ae677081cbccacfd39d9ed606879c5ce52
Crescenzo1995 <35402466+Crescenzo1995@users.noreply.github.com>
2018-01-21 15:15:52 +0100

    aggiunta classe Luogo

---
+++ default1/Luogo.java
@@ -0,0 +1,57 @@
+package default1;
+
+public class Luogo {
+	
+	//attributi
+	private String Nome;
+	private String Città;
+	private String Stato;
+	private String Indirizzo;
+
+	
+	//costruttore
+	public Luogo(String nome, String città, String stato, String indirizzo) {
+		Nome=nome;
+		Città=città;
+		Stato=stato;
+		Indirizzo=indirizzo;
+	}
+	
+	//getter
+	public String getNome() {
+		return Nome;
+	}
+	
+	public String getCittà() {
+		return Città;
+	}	
+	
+	public String getStato() {
+		return Stato;
+	}
+	
+	
+	public String getIndirizzo() {
+		return Indirizzo;
+	}
+	
+
+	//setter
+	public void setNome(String nome) {
+		Nome=nome;
+	}
+	
+	public void setCittà(String città) {
+		Città=città;
+	}	
+	
+	public void setStato(String stato) {
+		Stato=stato;
+	}
+	
+	
+	public void setIndirizzo(String indirizzo) {
+		Indirizzo=indirizzo;
+	}
+	
+}
