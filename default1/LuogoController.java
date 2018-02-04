68f6d3dde9f6b89940c8d6aec17af55e7df8e831
StefanoFal <35402477+StefanoFal@users.noreply.github.com>
2018-02-04 11:39:13 +0100

    Aggiunto il metodo che genera la statistica di Luogo

--- default1/LuogoController.java
+++ default1/LuogoController.java
@@ -1,8 +1,14 @@
 package default1;
 
+import java.awt.Color;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
+import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
+
+import org.jfree.chart.ChartPanel;
+import org.jfree.data.category.DefaultCategoryDataset;
 
 public class LuogoController {
 
@@ -92,5 +98,36 @@ public class LuogoController {
 			}
 		}
 	
+	public static void generaStatisticheLuogo(String NomeLuogo, String anno) {
+		List<Evento> Eventi=EventoController.cercaPerLuogo(NomeLuogo);
+		//3: dichiaro due array, uno per i valori e uno per mese e anno
+		String[] intervallo = new String[(int) 12];
+		double valori[]=new double[(int) 12];
+		int annocurr=Integer.parseInt(anno);
+		int mesecurr=1;
+		int num_eventi=0;
+		double ricavato=0;
+		//popolo l'array intervallo
+		for(int i=0;i<12;i++) {
+			intervallo[i]=mesecurr+"/"+annocurr;
+			mesecurr=mesecurr+1;
+		}
+		//popolo l'array con i valori che ottengo analizzando gli eventi inseriti per cui l'anno corrente corrisponde a in input
+		for(int j=0;j<12;j++) valori[j]=0;
+		long indice=0;
+		for(Evento currEvento:Eventi) {
+			num_eventi=num_eventi+1;
+			if(currEvento.getAnnoEvento()==annocurr) {
+				indice=currEvento.getMeseEvento()-1;
+				valori[(int) indice]=valori[(int) indice]+1;
+			}
+		}
+		for(int i=0;i<12;i++) System.out.println(valori[i]);
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset( ); 
+		for(int i=0;i<12;i++) dataset.addValue( valori[i] , "plot1" , intervallo[i]);  
+		LineChart chart= new LineChart( "", "Eventi ospitati nell'anno "+anno, "", "Mese", dataset);
+		ChartPanel chartPanel = new ChartPanel(chart.chart);
+		chartPanel.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
+	}
 	}
 	
