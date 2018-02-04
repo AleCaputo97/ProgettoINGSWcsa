de97e83285d37474a506506b938fa43281d8bfdb
StefanoFal <35402477+StefanoFal@users.noreply.github.com>
2018-02-04 17:11:50 +0100

    Creata la classe per la gestione delle statistiche generali, è un controller che ogni volta modifica la finestra utente

---
+++ default1/StatisticheGeneraliController.java
@@ -0,0 +1,41 @@
+package default1;
+import java.awt.Color;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableModel;
+
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+public class StatisticheGeneraliController {
+	
+	public static void TipoEventoPerLuogo () {
+		
+		String[] Tipologia = {"Concerto", "Evento sportivo", "Teatro", "Convegno", "Mostra","Altro"};
+		int[] Valori = {0,0,0,0,0,0};
+		
+		List<Evento> risultati = EventoDAO.cerca( "",  "",  0.0,  0.0, 0, "" ,  "");	
+		for(Evento curr:risultati) {
+			if (curr.getTipo().equals("Concerto"))
+				Valori[0]=Valori[0]+1;
+			if (curr.getTipo().equals("Evento sportivo"))
+				Valori[1]=Valori[1]+1;
+			if (curr.getTipo().equals("Teatro"))
+				Valori[2]=Valori[2]+1;
+			if (curr.getTipo().equals("Convegno"))
+				Valori[3]=Valori[3]+1;
+			if (curr.getTipo().equals("Mostra"))
+				Valori[4]=Valori[4]+1;
+			if (curr.getTipo().equals("Altro"))
+				Valori[5]=Valori[5]+1;
+				
+		}
+		PieChart statistica1 = new PieChart("Prova", 6, Tipologia, Valori);
+		
+	}
+	
+	
+}
