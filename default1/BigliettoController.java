7a269b0f8de7dd449a0c0f66e1c70ae313173a9d
StefanoFal <35402477+StefanoFal@users.noreply.github.com>
2018-02-06 18:06:29 +0100

    Indentato

--- default1/BigliettoController.java
+++ default1/BigliettoController.java
@@ -5,66 +5,54 @@ import java.util.List;
 
 public class BigliettoController {
 
-private static String normalizza (String string) {
-		
-		if (!(string.equals("")))
-			string = string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase();
-		
-		return string;
-		
+	private static String normalizza (String string) {	
+		if (!(string.equals(""))) string = string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase();
+		return string;	
 	}
 
-private static String normalizzaCF (String codicefiscale) {
-	
-	if (!(codicefiscale.equals("")))
-		codicefiscale = codicefiscale.toUpperCase();
-	
-	return codicefiscale;
-	
-}
+	private static String normalizzaCF (String codicefiscale) {
+		if (!(codicefiscale.equals(""))) codicefiscale = codicefiscale.toUpperCase();
+		return codicefiscale;
+	}
 	
 	
-public static List<Biglietto> bigliettiVendutiEvento(String evento) {
+	public static List<Biglietto> bigliettiVendutiEvento(String evento) {
 		evento=normalizza(evento);
-		return BigliettoDAO.cercaPerEvento(evento);
-		
+		return BigliettoDAO.cercaPerEvento(evento);	
 	}
 	
-public static List<Biglietto> bigliettiVendutiLuogo(String luogo) {
+	public static List<Biglietto> bigliettiVendutiLuogo(String luogo) {
 		luogo=normalizza(luogo);
-		return BigliettoDAO.cercaPerLuogo(luogo);
-		
+		return BigliettoDAO.cercaPerLuogo(luogo);	
 	}
 	
-public static boolean isBigliettiVendutiEvento(String evento) {
-	evento=normalizza(evento);
-	if (BigliettoDAO.cercaPerEvento(evento).isEmpty()) {
-		return false;
-	}else {
-		return true;
+	public static boolean isBigliettiVendutiEvento(String evento) {
+		evento=normalizza(evento);
+		if (BigliettoDAO.cercaPerEvento(evento).isEmpty()) {
+			return false;
+		}else {
+			return true;
+		}
 	}
-}
 
-public static boolean isBigliettiVendutiLuogo(String luogo) {
-	luogo=normalizza(luogo);
-	if (BigliettoDAO.cercaPerLuogo(luogo).isEmpty()) {
-		return false;
-	}else {
-		return true;
+	public static boolean isBigliettiVendutiLuogo(String luogo) {
+		luogo=normalizza(luogo);
+		if (BigliettoDAO.cercaPerLuogo(luogo).isEmpty()) {
+			return false;
+		}else {
+			return true;
+		}
 	}
-}
 
-public static List<Biglietto> bigliettiPerCliente(String cf){
-	
-	cf=normalizzaCF(cf);
-	return BigliettoDAO.cercaPerCodiceFiscale(cf);
-	
-}
+	public static List<Biglietto> bigliettiPerCliente(String cf){
+		cf=normalizzaCF(cf);
+		return BigliettoDAO.cercaPerCodiceFiscale(cf);
+	}
 
 
-public static void eliminaBiglietti(String CodiceFiscale) {
-	BigliettoDAO.eliminaBiglietti(CodiceFiscale);
-}
+	public static void eliminaBiglietti(String CodiceFiscale) {
+		BigliettoDAO.eliminaBiglietti(CodiceFiscale);
+	}
 
 }
 
