f487a0a4f999a8b0f30796592a588793eebf41aa
StefanoFal <35402477+StefanoFal@users.noreply.github.com>
2018-02-06 18:08:37 +0100

    Indentato

--- default1/BigliettoDAO.java
+++ default1/BigliettoDAO.java
@@ -27,26 +27,20 @@ public class BigliettoDAO {
 			        expressionAttributeValues);
 			         
 			       Iterator<Item> iterator = items.iterator();
-			   
 			       Item iteratorcurr;
-
 			       while (iterator.hasNext()) {
 			        iteratorcurr = iterator.next();
-			       // System.out.println("sto per eliminare:" + iteratorcurr);
 			        table.deleteItem("NumeroBiglietto", (String) iteratorcurr.get("NumeroBiglietto"));
 			       }
 	        }
 	 
 	 public static List<Biglietto> cercaPerEvento(String evento) {
-	       
-		 
 		   String tableName = "Biglietto";
 		   Iterator<Item> iterator = null;
 		   List<Biglietto> risultati=new ArrayList<Biglietto>();  
 		   Table table = ((DynamoDB) ProgettoINGSWcsa.connessione).getTable(tableName);
 		   Map<String, Object> expressionAttributeValues = new HashMap<String, Object>();
 		   expressionAttributeValues.put(":evento", evento);
-			  
 			    ItemCollection<ScanOutcome> items = table.scan (
 				        "Evento = :evento",                                  
 				        null,
@@ -57,23 +51,20 @@ public class BigliettoDAO {
 		   Item iteratorcurr;
 		   Biglietto curr;
 	       while (iterator.hasNext()) {
-	        iteratorcurr = iterator.next();
-	        curr=new Biglietto((String) iteratorcurr.get("NumeroBiglietto"),(String) iteratorcurr.get("CodFiscale"),(double) Double.parseDouble(iteratorcurr.get("Prezzo").toString()),(String) iteratorcurr.get("Luogo"),(String) iteratorcurr.get("Evento"), (String) iteratorcurr.get("DataAcquisto"));
-	        risultati.add(curr);
+	    	   iteratorcurr = iterator.next();
+	    	   curr=new Biglietto((String) iteratorcurr.get("NumeroBiglietto"),(String) iteratorcurr.get("CodFiscale"),(double) Double.parseDouble(iteratorcurr.get("Prezzo").toString()),(String) iteratorcurr.get("Luogo"),(String) iteratorcurr.get("Evento"), (String) iteratorcurr.get("DataAcquisto"));
+	    	   risultati.add(curr);
 	       }
 	   return risultati;
 	}
 	 
 	 public static List<Biglietto> cercaPerCodiceFiscale(String cf) {
-	       
-		 
 		   String tableName = "Biglietto";
 		   Iterator<Item> iterator = null;
 		   List<Biglietto> risultati=new ArrayList<Biglietto>();  
 		   Table table = ((DynamoDB) ProgettoINGSWcsa.connessione).getTable(tableName);
 		   Map<String, Object> expressionAttributeValues = new HashMap<String, Object>();
 		   expressionAttributeValues.put(":cf", cf);
-			  
 			    ItemCollection<ScanOutcome> items = table.scan (
 				        "CodFiscale = :cf",                                  
 				        null,
@@ -92,16 +83,13 @@ public class BigliettoDAO {
 	}
 	 
 	 public static List<Biglietto> cercaPerLuogo(String luogo) {
-	       
-		 
 		   String tableName = "Biglietto";
 		   Iterator<Item> iterator = null;
 		   List<Biglietto> risultati=new ArrayList<Biglietto>();  
 		   Table table = ((DynamoDB) ProgettoINGSWcsa.connessione).getTable(tableName);
 		   Map<String, Object> expressionAttributeValues = new HashMap<String, Object>();
 		   expressionAttributeValues.put(":luogo", luogo);
-			  
-			    ItemCollection<ScanOutcome> items = table.scan (
+		   ItemCollection<ScanOutcome> items = table.scan (
 				        "Luogo = :luogo",                                  
 				        null,
 				        null,                                          
