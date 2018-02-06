d70973f1ed3824bd972ff714fe03de405780e427
StefanoFal <35402477+StefanoFal@users.noreply.github.com>
2018-02-06 18:35:45 +0100

    Indentato

--- default1/PieChart.java
+++ default1/PieChart.java
@@ -6,23 +6,18 @@ import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.general.PieDataset;
 
 public class PieChart {
-	
 	JFreeChart chart;
 	
 	public PieChart (String nome, int numerospicchi, String spicchio[], int valore[]) {
-		
 		 DefaultPieDataset dataset = new DefaultPieDataset( );
 	     int i;
-	     for (i=0; i<numerospicchi; i++)
-	    	 dataset.setValue( spicchio[i] , valore[i] );  
-	     
+	     for (i=0; i<numerospicchi; i++) dataset.setValue( spicchio[i] , valore[i] );  
 	     chart = ChartFactory.createPieChart(      
 		         nome,   
 		         dataset,             
 		         true,       
 		         true, 
 		         false);
-		
 	}
 
 }
