92fc55ebcab203a6d4056cff56f646a7377a4fca
StefanoFal <35402477+StefanoFal@users.noreply.github.com>
2018-02-06 17:48:59 +0100

    Nuova classe BarChart

---
+++ default1/BarChart.java
@@ -0,0 +1,31 @@
+package default1;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+public class BarChart extends ApplicationFrame {
+	
+	JFreeChart chart;
+   
+   public BarChart( String applicationTitle , String chartTitle, String assex, String assey, DefaultCategoryDataset dataset) {
+      super( applicationTitle );        
+      JFreeChart barChart = ChartFactory.createBarChart( //asse x ha categorie, asse y ha lo score assegnato
+         chartTitle,           
+         assex,            
+         assey,            
+         dataset,          
+         PlotOrientation.VERTICAL,           
+         true, true, false);
+         
+      chart = barChart;
+      
+      ChartPanel chartPanel = new ChartPanel( barChart );        
+      chartPanel.setPreferredSize(new java.awt.Dimension( 560 , 367 ) );        
+      setContentPane( chartPanel ); 
+   }
+}
