3b2f79a93baf3316becae80b5571721200a87b07
StefanoFal <35402477+StefanoFal@users.noreply.github.com>
2018-02-06 18:12:23 +0100

    Indentato

--- default1/ClienteController.java
+++ default1/ClienteController.java
@@ -20,7 +20,6 @@ import com.amazonaws.services.dynamodbv2.document.Table;
 
 public class ClienteController {
     
-    
 	private static String normalizza (String string) {
 		if (!(string.equals(""))) string = string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase();
 		return string;
@@ -79,26 +78,21 @@ public class ClienteController {
 		Pattern p= Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 		//Controllo iniziale: se c'è un campo vuoto in un inserimento questi deve essere impedito
 		if((nome.equals("")||cognome.equals("")||email.equals("")||codicefiscale.equals("")||data.equals(""))) {
-			FinestraUtente.messaggio.setText("<html><font color=\"red\">ERRORE: Almeno uno dei campi è vuoto </font></html>");
-		}
-			//Controllo successivo: mail ben posta e codice fiscale di 16 caratteri
-			else if(codicefiscale.length()!=16) {
+				FinestraUtente.messaggio.setText("<html><font color=\"red\">ERRORE: Almeno uno dei campi è vuoto </font></html>");
+		}else if(codicefiscale.length()!=16) {//Controllo successivo: mail ben posta e codice fiscale di 16 caratteri
 				FinestraUtente.messaggio.setText("<html><font color=\"red\">ERRORE: Il codice fiscale non è di 16 caratteri </font></html>");
-			}
-			else if(!p.matcher(email).matches()) {
+		}else if(!p.matcher(email).matches()) {
 				FinestraUtente.messaggio.setText("<html><font color=\"red\">ERRORE: la mail non è nel formato testo@dominio.testo </font></html>");
+		}else {
+				nome=normalizza(nome);
+				cognome=normalizza(cognome);
+				email=normalizzaEmail(email);
+				codicefiscale=normalizzaCF(codicefiscale);
+				ClienteDAO.inserisciModifica(nome, cognome, email, codicefiscale, data);
+				FinestraUtente.clienteClear.doClick();
+				FinestraUtente.messaggio.setText("<html><font color=\"green\">Cliente inserito correttamente </font></html>");
 			}
-			else {
-			nome=normalizza(nome);
-			cognome=normalizza(cognome);
-			email=normalizzaEmail(email);
-			codicefiscale=normalizzaCF(codicefiscale);
-			ClienteDAO.inserisciModifica(nome, cognome, email, codicefiscale, data);
-			FinestraUtente.clienteClear.doClick();
-			FinestraUtente.messaggio.setText("<html><font color=\"green\">Cliente inserito correttamente </font></html>");
-			}
-
-		}
+	}
 	
 	public static boolean modifica (String nome, String cognome, String email, String codicefiscale, String data) {
 		
@@ -108,27 +102,22 @@ public class ClienteController {
 		if((nome.equals("")||cognome.equals("")||email.equals("")||codicefiscale.equals("")||data.equals(""))) {
 				FinestraUtente.messaggio.setText("<html><font color=\"red\">ERRORE: Almeno uno dei campi è vuoto </font></html>");
 				return false;
-		}
-			//Controllo successivo: mail ben posta e codice fiscale di 16 caratteri
-			else if(codicefiscale.length()!=16) {
+		}else if(codicefiscale.length()!=16) {//Controllo successivo: mail ben posta e codice fiscale di 16 caratteri
 				FinestraUtente.messaggio.setText("<html><font color=\"red\">ERRORE: Il codice fiscale non è di 16 caratteri </font></html>");
 				return false;
-			}
-			else if(!p.matcher(email).matches()) {
+		}else if(!p.matcher(email).matches()) {
 				FinestraUtente.messaggio.setText("<html><font color=\"red\">ERRORE: la mail non è nel formato testo@dominio.testo </font></html>");
 				return false;
+		}else{
+				nome=normalizza(nome);
+				cognome=normalizza(cognome);
+				email=normalizzaEmail(email);
+				codicefiscale=normalizzaCF(codicefiscale);
+				ClienteDAO.inserisciModifica(nome, cognome, email, codicefiscale, data);
+				FinestraUtente.clienteClear.doClick();
+				return true;
 			}
-			else {
-			nome=normalizza(nome);
-			cognome=normalizza(cognome);
-			email=normalizzaEmail(email);
-			codicefiscale=normalizzaCF(codicefiscale);
-			ClienteDAO.inserisciModifica(nome, cognome, email, codicefiscale, data);
-			FinestraUtente.clienteClear.doClick();
-			return true;
-			}
-
-		}
+	}
 	
 	public static void elimina(String CodiceFiscale) {
 		ClienteDAO.elimina(CodiceFiscale);
