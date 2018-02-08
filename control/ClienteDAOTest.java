a5dc809dcbc39d8bc3aaaa9ae6c8c7e62a13e98a
StefanoFal <35402477+StefanoFal@users.noreply.github.com>
2018-02-08 17:57:17 +0100

    inseriti i metodi di before e after

--- control/ClienteDAOTest.java
+++ control/ClienteDAOTest.java
@@ -5,28 +5,50 @@ package control;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 
 /**
  * @author cresc
  *
  */
 class ClienteDAOTest {
-
+    static AmazonDynamoDB dynamoDB;
+    private static DynamoDB connessione;
+    private ClienteDAO clienteDAO;
 	/**
 	 * @throws java.lang.Exception
 	 */
-	@BeforeEach
+	@Before
 	void setUp() throws Exception {
+		ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+        try {
+            credentialsProvider.getCredentials();
+        } catch (Exception e) {
+        }
+        dynamoDB = AmazonDynamoDBClientBuilder.standard()
+            .withCredentials(credentialsProvider)
+            .withRegion("eu-central-1")
+            .build();
+		connessione = new DynamoDB(dynamoDB);
+	    clienteDAO=new ClienteDAO(connessione);
+		clienteDAO.inserisciModifica("Michele", "Caparezza", "michelecaparezza@gmail.com", "QWERTYUIOPLKJHGF", "9 ottobre 1973");
 	}
 
 	/**
 	 * @throws java.lang.Exception
 	 */
-	@AfterEach
+	@After
 	void tearDown() throws Exception {
+		clienteDAO.elimina("QWERTYUIOPLKJHGF");
 	}
 
 	@Test
