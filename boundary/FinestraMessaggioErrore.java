c9d40c1d3fc0183b6350a05aa870c2be15d5f4e7
StefanoFal <35402477+StefanoFal@users.noreply.github.com>
2018-02-20 16:36:38 +0100

    Aggiunto frame che gestisce il messaggio di errore

---
+++ boundary/FinestraMessaggioErrore.java
@@ -0,0 +1,49 @@
+package boundary;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class FinestraMessaggioErrore extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FinestraMessaggioErrore frame = new FinestraMessaggioErrore();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	 * Create the frame.
+	 */
+	public FinestraMessaggioErrore(Exception E) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 563, 329);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("ERRORE: "+E.getMessage());
+		lblNewLabel.setBounds(65, 80, 381, 46);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Se il problema dovesse persistere, scrivere a stefanofalangone@gmail.com");
+		lblNewLabel_1.setBounds(45, 144, 479, 68);
+		contentPane.add(lblNewLabel_1);
+	}
+}
