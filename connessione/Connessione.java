90e66cf5d5377f1daeb68e0c2dec0d220d436ec1
StefanoFal <35402477+StefanoFal@users.noreply.github.com>
2018-02-20 16:37:19 +0100

    Aggiunto catch delle eccezioni

--- connessione/Connessione.java
+++ connessione/Connessione.java
@@ -23,25 +23,24 @@ public class Connessione {
     private static DynamoDB connessione;
     
 	public static void main(String[] args) throws Exception {
-		 try 
-		    { 
-			  UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-		    } 
-		    catch(Exception e){ 
-		    }
-		init();
-		connessione = new DynamoDB(dynamoDB);
-		LuogoDAO luogoDAO=new LuogoDAO(connessione);
-		LuogoController luogoController=new LuogoController(luogoDAO);
-		ClienteDAO clienteDAO=new ClienteDAO(connessione);
-		ClienteController clienteController=new ClienteController(clienteDAO);
-		EventoDAO eventoDAO=new EventoDAO(connessione);
-		EventoController eventoController=new EventoController(eventoDAO);
-		BigliettoDAO bigliettoDAO=new BigliettoDAO(connessione);
-		BigliettoController bigliettoController=new BigliettoController(bigliettoDAO);
-		FinestraUtente window = new FinestraUtente();
-		window.frmProgettoingswcsa.setVisible(true);
-		
+	    try{ 
+	    	UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+	    	init();
+	    	connessione = new DynamoDB(dynamoDB);
+	    	LuogoDAO luogoDAO=new LuogoDAO(connessione);
+	    	LuogoController luogoController=new LuogoController(luogoDAO);
+	    	ClienteDAO clienteDAO=new ClienteDAO(connessione);
+	    	ClienteController clienteController=new ClienteController(clienteDAO);
+	    	EventoDAO eventoDAO=new EventoDAO(connessione);
+	    	EventoController eventoController=new EventoController(eventoDAO);
+			BigliettoDAO bigliettoDAO=new BigliettoDAO(connessione);
+			BigliettoController bigliettoController=new BigliettoController(bigliettoDAO);
+			FinestraUtente window = new FinestraUtente();
+			window.frmProgettoingswcsa.setVisible(true);
+		}catch(Exception e){ 
+			FinestraMessaggioErrore errore=new FinestraMessaggioErrore(e);
+			errore.setVisible(true);
+	    }
 	}
 
     public static void init() throws Exception {
